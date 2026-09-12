df6c1f3db72abcbf70a5ae5b8048b5ddd366fcad
Udemezue John <johnzeus14@gmail.com>
2026-09-12 08:57:29 +0100

    Added dot env

--- src/leeto/leeto/settings.py
+++ src/leeto/leeto/settings.py
@@ -15,12 +15,20 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+from dotenv import load_dotenv
+
+# Load the .env file
+load_dotenv()
+import os
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-%c#r+ai7xdmm1of%b#uv+u5gc$&wb7t_irz7o(*dl0v06lb&d9'
+SECRET_KEY = os.getenv('SECRET_KEY')
+
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
